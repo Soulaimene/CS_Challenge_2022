b6169d6bae3dec7047e36e3132cd7eace6307e07
dhouib-akram <93084779+dhouib-akram@users.noreply.github.com>
2023-04-05 23:09:26 +0100

    Update unet_models.py

--- unet_models.py
+++ unet_models.py
@@ -69,7 +69,84 @@ class Models(object):
             print("model not selected yet")
         
 
-        
+def unet0(KERN_SIZE=3, ACT='relu', FILTERS=[16,24,32,48,64,96,128,128]):
+    #ACT = 'relu'
+    #KERN_SIZE = 3
+    #FILTERS=[16,24,32,48,64,96,128,128]
+    
+    z1 = Input(shape=(None,None,1))
+    print('z1: {}'.format(z1.shape))
+
+    z2 = Conv2D(FILTERS[0], KERN_SIZE, padding='same', activation=ACT)(z1)
+    p2 = AveragePooling2D(pool_size=2)(z2)
+    print('z2: {}, \np2: {}'.format(z2.shape, p2.shape))
+
+    z3 = Conv2D(FILTERS[1], KERN_SIZE, padding='same', activation=ACT)(p2)
+    p3 = AveragePooling2D(pool_size=2)(z3)
+    print('z3: {}, \np3: {}'.format(z3.shape, p3.shape))
+
+    z4 = Conv2D(FILTERS[2], KERN_SIZE, padding='same', activation=ACT)(p3)
+    d4 = Dropout(0.2)(z4)
+    p4 = AveragePooling2D(pool_size=2)(d4)
+    print('z4: {}, \np4: {}'.format(z4.shape, p4.shape))
+
+    z5 = Conv2D(FILTERS[3], KERN_SIZE, padding='same', activation=ACT)(p4)
+    d5 = Dropout(0.2)(z5)
+    p5 = AveragePooling2D(pool_size=2)(d5)
+    print('z5: {}'.format(z5.shape))
+
+    z6 = Conv2D(FILTERS[4], KERN_SIZE, padding='same', activation=ACT)(p5)
+    d6 = Dropout(0.3)(z6)
+    p6 = AveragePooling2D(pool_size=2)(d6)
+
+    z7 = Conv2D(FILTERS[5], KERN_SIZE, padding='same', activation=ACT)(p6)
+    d7 = Dropout(0.4)(z7)
+    p7 = AveragePooling2D(pool_size=2)(d7)
+
+    z8 = Conv2D(FILTERS[6], KERN_SIZE, padding='same', activation=ACT)(p7)
+    d8 = Dropout(0.5)(z8)
+    p8 = AveragePooling2D(pool_size=2)(d8)
+
+    z9 = Conv2D(FILTERS[7], KERN_SIZE, padding='same', activation=ACT)(p8)
+    d9 = Dropout(0.5)(z9)
+
+    u9 = UpSampling2D(size=2)(d9)
+    q9 = Conv2D(FILTERS[6], KERN_SIZE, padding='same', activation=ACT)(u9)
+    d9b = Dropout(0.5)(q9)
+    a9 = Concatenate()([d9b,z8])
+
+    u8 = UpSampling2D(size=2)(a9)
+    q8 = Conv2D(FILTERS[5], KERN_SIZE, padding='same', activation=ACT)(u8)
+    d8b = Dropout(0.4)(q8)
+    a8 = Concatenate()([d8b,z7])
+
+    u7 = UpSampling2D(size=2)(a8)
+    q7 = Conv2D(FILTERS[4], KERN_SIZE, padding='same', activation=ACT)(u7)
+    d7b = Dropout(0.3)(q7)
+    a7 = Concatenate()([d7b,z6])
+
+    u6 = UpSampling2D(size=2)(a7)
+    q6 = Conv2D(FILTERS[3], KERN_SIZE, padding='same', activation='relu')(u6)
+    d6b = Dropout(0.2)(q6)
+    a6 = Concatenate()([d6b,z5])
+
+    u5 = UpSampling2D(size=2)(a6)
+    q5 = Conv2D(FILTERS[2], KERN_SIZE, padding='same', activation=ACT)(u5)
+    d5b = Dropout(0.2)(q5)
+    a5 = Concatenate()([d5b,z4])
+
+    u4 = UpSampling2D(size=2)(a5)
+    q4 = Conv2D(FILTERS[1], KERN_SIZE, padding='same', activation=ACT)(u4)
+    a4 = Concatenate()([q4,z3])
+
+    u3 = UpSampling2D(size=2)(a4)
+    q3 = Conv2D(FILTERS[0], KERN_SIZE, padding='same', activation=ACT)(u3)
+    a3 = Concatenate()([q3,z2])
+
+    z_final = Conv2D(1, KERN_SIZE, padding='same', activation='sigmoid')(a3)
+    #z8 activation = sigmoid or softmax
+
+    return Model(inputs = z1, outputs = z_final)        
 def unet1(): 
     ACT = 'relu'
     KERN_SIZE = 3
@@ -228,7 +305,85 @@ def unet2():
 
     return Model(inputs = z1, outputs = z_final)
 
+def unet3():
+    ACT = 'relu'
+    KERN_SIZE = 3
 
+    FILTERS=[8,12,16,24,32,48,64,64]
+    
+    z1 = Input(shape=(None,None,1))
+    print('z1: {}'.format(z1.shape))
+
+    z2 = Conv2D(FILTERS[0], KERN_SIZE, padding='same', activation=ACT)(z1)
+    p2 = AveragePooling2D(pool_size=2)(z2)
+    print('z2: {}, \np2: {}'.format(z2.shape, p2.shape))
+
+    z3 = Conv2D(FILTERS[1], KERN_SIZE, padding='same', activation=ACT)(p2)
+    p3 = AveragePooling2D(pool_size=2)(z3)
+    print('z3: {}, \np3: {}'.format(z3.shape, p3.shape))
+
+    z4 = Conv2D(FILTERS[2], KERN_SIZE, padding='same', activation=ACT)(p3)
+    d4 = Dropout(0.2)(z4)
+    p4 = AveragePooling2D(pool_size=2)(d4)
+    print('z4: {}, \np4: {}'.format(z4.shape, p4.shape))
+
+    z5 = Conv2D(FILTERS[3], KERN_SIZE, padding='same', activation=ACT)(p4)
+    d5 = Dropout(0.2)(z5)
+    p5 = AveragePooling2D(pool_size=2)(d5)
+    print('z5: {}'.format(z5.shape))
+
+    z6 = Conv2D(FILTERS[4], KERN_SIZE, padding='same', activation=ACT)(p5)
+    d6 = Dropout(0.3)(z6)
+    p6 = AveragePooling2D(pool_size=2)(d6)
+
+    z7 = Conv2D(FILTERS[5], KERN_SIZE, padding='same', activation=ACT)(p6)
+    d7 = Dropout(0.4)(z7)
+    p7 = AveragePooling2D(pool_size=2)(d7)
+
+    z8 = Conv2D(FILTERS[6], KERN_SIZE, padding='same', activation=ACT)(p7)
+    d8 = Dropout(0.5)(z8)
+    p8 = AveragePooling2D(pool_size=2)(d8)
+
+    z9 = Conv2D(FILTERS[7], KERN_SIZE, padding='same', activation=ACT)(p8)
+    d9 = Dropout(0.5)(z9)
+
+    u9 = UpSampling2D(size=2)(d9)
+    q9 = Conv2D(FILTERS[6], KERN_SIZE, padding='same', activation=ACT)(u9)
+    d9b = Dropout(0.5)(q9)
+    a9 = Concatenate()([d9b,z8])
+
+    u8 = UpSampling2D(size=2)(a9)
+    q8 = Conv2D(FILTERS[5], KERN_SIZE, padding='same', activation=ACT)(u8)
+    d8b = Dropout(0.4)(q8)
+    a8 = Concatenate()([d8b,z7])
+
+    u7 = UpSampling2D(size=2)(a8)
+    q7 = Conv2D(FILTERS[4], KERN_SIZE, padding='same', activation=ACT)(u7)
+    d7b = Dropout(0.3)(q7)
+    a7 = Concatenate()([d7b,z6])
+
+    u6 = UpSampling2D(size=2)(a7)
+    q6 = Conv2D(FILTERS[3], KERN_SIZE, padding='same', activation='relu')(u6)
+    d6b = Dropout(0.2)(q6)
+    a6 = Concatenate()([d6b,z5])
+
+    u5 = UpSampling2D(size=2)(a6)
+    q5 = Conv2D(FILTERS[2], KERN_SIZE, padding='same', activation=ACT)(u5)
+    d5b = Dropout(0.2)(q5)
+    a5 = Concatenate()([d5b,z4])
+
+    u4 = UpSampling2D(size=2)(a5)
+    q4 = Conv2D(FILTERS[1], KERN_SIZE, padding='same', activation=ACT)(u4)
+    a4 = Concatenate()([q4,z3])
+
+    u3 = UpSampling2D(size=2)(a4)
+    q3 = Conv2D(FILTERS[0], KERN_SIZE, padding='same', activation=ACT)(u3)
+    a3 = Concatenate()([q3,z2])
+
+    z_final = Conv2D(1, KERN_SIZE, padding='same', activation='sigmoid')(a3)
+    #z8 activation = sigmoid or softmax
+
+    return Model(inputs = z1, outputs = z_final)
 def unet4():
     ACT = 'relu'
     KERN_SIZE = 3
